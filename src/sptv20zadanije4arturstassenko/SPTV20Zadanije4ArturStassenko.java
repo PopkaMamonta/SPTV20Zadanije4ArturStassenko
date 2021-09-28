8a604ff0256d7592d818f8671ff043d85a862a1d
Pomeloed <38099051+Pomeloed@users.noreply.github.com>
2021-09-28 14:30:39 +0300

    final version

--- src/sptv20zadanije4arturstassenko/SPTV20Zadanije4ArturStassenko.java
+++ src/sptv20zadanije4arturstassenko/SPTV20Zadanije4ArturStassenko.java
@@ -11,9 +11,41 @@ public class SPTV20Zadanije4ArturStassenko {
         int[] nums;
         nums=new int[10];
         for (int i = 0; i < nums.length; i++) {
-            nums[i]=random.nextInt(100);
+            for (int j = 0; j < 2; j++) {
+            int rand=random.nextInt(100);
+            if(rand%2==0){
+                nums[i]=rand;
+                j++;
+            }else{
+                j-=1;
+            }
+            }
         }
+        for (int i = 0; i < nums.length; i++) {
+            int min=nums[i];
+            int place=i;
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[j]<min){
+                    min=nums[j];
+                    place=j;
+                }
+            }
+                if (i != place) {
+                    int tmp = nums[i];
+                    nums[i] = nums[place];
+                    nums[place] = tmp;
+                }
+        }
+        double sum=0;
+        double avg=0;
+        for (int i = 0; i < nums.length; i++) {
+            sum+=nums[i];
+            
+            
+        }
+        avg=sum/nums.length;
         System.out.println("nums = "+Arrays.toString(nums));
+        System.out.printf("nums average = %.2f%n",avg);
     }
     
 }
